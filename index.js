00be86bfcc91e72dd11ef1b97ae774521ede9cfd
Sovch <Filin_999@bk.ru>
2022-06-21 19:35:10 +0500

    add correct url for parsers in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import { parser } from './parsers/parsers.js'
+import { parser } from './bin/parsers/parsers.js'
 import { dissimilarities } from './bin/findDissimilarities.js'
 
 const __filename = fileURLToPath(import.meta.url);
