95176b6e1a80eea8308eddfef5d216f7519bbc9c
Sovch <Filin_999@bk.ru>
2022-06-21 19:44:20 +0500

    one more fix

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname, join } from 'path';
 import { parser } from './bin/parsers/parsers.js'
 import { dissimilarities } from './bin/findDissimilarities.js'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getAbsolutePath = (filename) => join(__dirname, filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// const getAbsolutePath = (filename) => join(__dirname, filename);
+
+const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const getFileData = (filepath) => {
   const absoluteFilePath = getAbsolutePath(filepath);
-  return fs.readFileSync(absoluteFilePath, 'utf-8').toString();
+  return fs.readFileSync(absoluteFilePath, 'utf-8');
 };
+
+// const getFileData = (filepath) => {
+//   const absoluteFilePath = getAbsolutePath(filepath);
+//   return fs.readFileSync(absoluteFilePath, 'utf-8').toString();
+// };
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
 	const content1 = getFileData(filepath1);
