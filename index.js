6c45ef77eb17f222cd46c30e5609d6d0f9ad66d0
Sovch <Filin_999@bk.ru>
2022-06-21 18:03:42 +0500

    add another path for hexlet check

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import fs from 'fs';
 
 import { dissimilarities } from './bin/findDissimilarities.js'
 
 const gendiff = (path1, path2, formatter = 'stylish') => {
-	const filePath1 = path.resolve(path1);
-	const filePath2 = path.resolve(path2);
-	const data1 = fs.readFileSync(filePath1).toString();
-	const data2 = fs.readFileSync(filePath2).toString();
 
+	const __filename = fileURLToPath(import.meta.url);
+	const __dirname = dirname(__filename);
+	const getFixturePath = (filename) => join(__dirname, '__fixtures__', filename);
+
+	const file1 = getFixturePath(path1);
+	const data1 = fs.readFileSync(file1).toString();
+	const file2 = getFixturePath(path2);
+	const data2 = fs.readFileSync(file2).toString();
 
 	const diff = dissimilarities(data1, data2, formatter);
 	return diff;
