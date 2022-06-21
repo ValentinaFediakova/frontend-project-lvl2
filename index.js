d40b07a1f7b8932ed7de22ca1795319293e7c69a
Sovch <Filin_999@bk.ru>
2022-06-21 16:10:47 +0500

    another fix for hexlet-check

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { dissimilarities } from './bin/findDissimilarities.js'
 
-export const gendiff = (data1, data2, formatter = 'stylish') => {
+const gendiff = (data1, data2, formatter = 'stylish') => {
 	const diff = dissimilarities(data1, data2, formatter);
 	return diff;
 }
+
+export default gendiff;
